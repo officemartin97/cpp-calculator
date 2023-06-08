8c9cf5cd39ab12853cca9c38a39a4057d552c50c
officemartin97 <officemartin97@gmail.com>
2023-06-08 19:48:34 +0200

    can exit with char n and can redo with char y

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-#include <iostream>
-
 void clearScreen()
 {
     std::cout << "\033[2J\033[1;1H"; // ANSI escape sequence to clear screen
@@ -15,6 +13,8 @@ int main()
 
     do
     {
+        clearScreen(); // Clear the screen before each calculation
+
         std::cout << "This is a simple calculator!" << std::endl
                   << std::endl;
 
@@ -30,7 +30,7 @@ int main()
         std::cout << "These are the options for operations which you can choose from:" << std::endl;
         std::cout << "1. Add together +" << std::endl;
         std::cout << "2. Subtract them -" << std::endl;
-        std::cout << "3. Multiplicate them *" << std::endl;
+        std::cout << "3. Multiply them *" << std::endl;
         std::cout << "4. Divide them /" << std::endl;
         std::cin >> operation;
 
@@ -53,21 +53,30 @@ int main()
         else
         {
             std::cout << "Exception: Unavailable option!" << std::endl;
-            continue; // Ugrás vissza a kezdethez, és újraindítja a ciklust
+            continue; // Jump back to the beginning of the loop and restart
         }
 
         std::cout << "The result is: " << result << std::endl;
 
-        std::cout << "Do you want to perform another calculation? (Y/N): ";
-        std::cin >> choice;
-
-        if (choice != 'Y' && choice != 'y')
+        do
         {
-            clearScreen();
-            break; // Kilépés a ciklusból, ha a válasz nem 'Y' vagy 'y'
-        }
+            std::cout << "Do you want to perform another calculation? (Y/N): ";
+            std::cin >> choice;
 
-        clearScreen();
+            if (choice == 'Y' || choice == 'y')
+            {
+                break; // Exit the inner loop and continue with a new calculation
+            }
+            else if (choice == 'N' || choice == 'n')
+            {
+                clearScreen();
+                return 0; // Exit the program if the answer is 'N' or 'n'
+            }
+            else
+            {
+                std::cout << "Invalid input. Please enter 'Y' or 'N'." << std::endl;
+            }
+        } while (true);
 
     } while (true);
 
